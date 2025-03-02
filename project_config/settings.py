c19a3bcf68615453f6ffa041c3a247c66bd50cae
ShinyaKato <howanpin5525@gmail.com>
2025-03-02 13:22:34 +0900

    [バグ修正]webサーバーを使って起動するとstaticファイルが読み込めない問題を修正

--- project_config/settings.py
+++ project_config/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'project_config.urls'
@@ -124,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"testApp1", "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
